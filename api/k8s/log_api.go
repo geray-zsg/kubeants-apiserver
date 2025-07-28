7b464ab04a5ccb7880772a5225616956c9289984
geray <1690014753@qq.com>
2025-07-28 15:25:15 +0800

    优化了日志接口，支持下载

--- api/k8s/log_api.go
+++ api/k8s/log_api.go
@@ -2,6 +2,7 @@ package k8s
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -11,29 +12,38 @@ import (
 type LogApi struct{}
 
 // GET /gapi/cluster/:cluster/workspace/:workspace/api/v1/namespaces/:namespace/pods/:pod/log
+// GET /gapi/.../pods/nginx/log?container=nginx  查看日志
+// window.open(`/gapi/.../pods/nginx/log?container=nginx&download=true`)	下载日志
 func (*LogApi) GetPodLogs(c *gin.Context) {
 	ctx := context.TODO()
 	namespace := c.Param("namespace")
 	pod := c.Param("pod")
 
-	// 可选参数
-	container := c.Query("container") // 支持多容器指定
+	container := c.Query("container")
 	tailStr := c.DefaultQuery("tailLines", "100")
 	followStr := c.DefaultQuery("follow", "false")
 
 	tailLines, err := strconv.ParseInt(tailStr, 10, 64)
 	if err != nil {
-		tailLines = 100
+		tailLines = 10000
 	}
 
 	follow := followStr == "true"
 
-	// logService := k8s.LogService{}
 	logs, err := logService.GetPodLogs(ctx, namespace, pod, container, tailLines, follow)
 	if err != nil {
 		response.FailWithMessage(c, err.Error())
 		return
 	}
 
+	// 下载模式
+	if c.Query("download") == "true" {
+		c.Header("Content-Disposition", fmt.Sprintf("attachment; filename=%s.log", pod))
+		c.Header("Content-Type", "text/plain; charset=utf-8")
+		c.String(200, logs)
+		return
+	}
+
+	// 普通展示
 	c.String(200, logs)
 }
